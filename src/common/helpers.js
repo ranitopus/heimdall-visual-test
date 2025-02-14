960b98617c795dae3fe6283a822b11b652336691
Raniere de Souza Santos <ranieresantos@ntopus.com.br>
2025-02-14 18:04:41 -0300

    Adding validation and some sad paths in the testing of the rgbToHsl function

--- src/common/helpers.js
+++ src/common/helpers.js
@@ -1,15 +1,28 @@
+function isValidRgbVector(value) {
+  if (!value) return TypeError('value should not be falsy/empty')
+  if (!Array.isArray(value)) return TypeError('value should be an array')
+  if (value.length !== 3) return RangeError('value should have exactly 3 items')
+  if (value.some(item => !Number.isInteger(item))) return TypeError('some item in value is not an integer number')
+  if (value.some(item => item < 0)) return RangeError('some item in value is not a positive integer')
+  if (value.some(item => item > 255)) return RangeError('some item in value is over 255')
+
+  return true
+}
+
 /**
  * Sources used for research to implement this function:
  * - https://css-tricks.com/converting-color-spaces-in-javascript/#aa-rgb-to-hsl
  * - https://www.30secondsofcode.org/js/s/rgb-to-hsl/
  * - https://en.wikipedia.org/wiki/HSL_and_HSV
  */
-export function rgbToHsl([r, g, b]) {
+export function rgbToHsl(rgbVector) {
+  const result = isValidRgbVector(rgbVector)
+  if (result !== true) throw result
+
+  let [ r, g, b ] = rgbVector
   r = r / 255; g = g / 255; b = b / 255
 
-  const min = Math.min(r, g, b),
-        max = Math.max(r, g, b),
-        delta = max - min
+  const min = Math.min(r, g, b), max = Math.max(r, g, b), delta = max - min
   let h = 0, s = 0, l = 0
 
   if (delta === 0)
@@ -23,7 +36,7 @@ export function rgbToHsl([r, g, b]) {
 
   l = (max + min) / 2
 
-  s = (delta == 0) ? 0 : delta / (1 - Math.abs(2 * l - 1))
+  s = (delta === 0) ? 0 : delta / (1 - Math.abs(2 * l - 1))
 
   return [Math.round(h * 60), Math.round(s * 100), Math.round(l * 100)]
 }
--- src/common/helpers.test.js
+++ src/common/helpers.test.js
@@ -26,41 +26,53 @@ describe('function #imgBase64ToDataUrl', () => {
     // Arrange
     const inputA = 'abcd=', inputB = 'abcd==', inputC = 'abcd==='
     // Act
-    const resultA = imgBase64ToDataUrl(inputA)
-    const resultB = imgBase64ToDataUrl(inputB)
+    const resultA  = imgBase64ToDataUrl(inputA),
+          resultB  = imgBase64ToDataUrl(inputB),
+          attemptC = () => imgBase64ToDataUrl(inputC)
     // Assert
     expect(resultA).toMatch(new RegExp(`^data:image/[a-z]{3,4};base64,${inputA}`))
     expect(resultB).toMatch(new RegExp(`^data:image/[a-z]{3,4};base64,${inputB}`))
-    expect(() => imgBase64ToDataUrl(inputC)).toThrow(TypeError, 'argument should be a valid base64 string')
+    expect(attemptC).toThrow(TypeError, 'argument should be a valid base64 string')
   })
 
   it('should not accept a non-string input', () => {
     // Arrange
     const inputA = false, inputB = 0, inputC = {}
+    // Act
+    const attemptA = () => imgBase64ToDataUrl(inputA),
+          attemptB = () => imgBase64ToDataUrl(inputB),
+          attemptC = () => imgBase64ToDataUrl(inputC)
     // Assert
-    expect(() => imgBase64ToDataUrl(inputA)).toThrow(TypeError, 'argument should be a string')
-    expect(() => imgBase64ToDataUrl(inputB)).toThrow(TypeError, 'argument should be a string')
-    expect(() => imgBase64ToDataUrl(inputC)).toThrow(TypeError, 'argument should be a string')
+    expect(attemptA).toThrow(TypeError, 'argument should be a string')
+    expect(attemptB).toThrow(TypeError, 'argument should be a string')
+    expect(attemptC).toThrow(TypeError, 'argument should be a string')
   })
 
   it('should not accept an invalid base64 string as argument', () => {
     // Arrange
     const inputA = '',    inputB = 'not.a;base64', inputC = 'çÁëỳ',
           inputD = '-_+', inputE = '=abcd',        inputF = 'ab=cd'
+    // Act
+    const attemptA = () => imgBase64ToDataUrl(inputA),
+          attemptB = () => imgBase64ToDataUrl(inputB),
+          attemptC = () => imgBase64ToDataUrl(inputC),
+          attemptD = () => imgBase64ToDataUrl(inputD),
+          attemptE = () => imgBase64ToDataUrl(inputE),
+          attemptF = () => imgBase64ToDataUrl(inputF)
     // Assert
-    expect(() => imgBase64ToDataUrl(inputA)).toThrow(TypeError, 'argument should be a valid base64 string')
-    expect(() => imgBase64ToDataUrl(inputB)).toThrow(TypeError, 'argument should be a valid base64 string')
-    expect(() => imgBase64ToDataUrl(inputC)).toThrow(TypeError, 'argument should be a valid base64 string')
-    expect(() => imgBase64ToDataUrl(inputD)).toThrow(TypeError, 'argument should be a valid base64 string')
-    expect(() => imgBase64ToDataUrl(inputE)).toThrow(TypeError, 'argument should be a valid base64 string')
-    expect(() => imgBase64ToDataUrl(inputF)).toThrow(TypeError, 'argument should be a valid base64 string')
+    expect(attemptA).toThrow(TypeError, 'argument should be a valid base64 string')
+    expect(attemptB).toThrow(TypeError, 'argument should be a valid base64 string')
+    expect(attemptC).toThrow(TypeError, 'argument should be a valid base64 string')
+    expect(attemptD).toThrow(TypeError, 'argument should be a valid base64 string')
+    expect(attemptE).toThrow(TypeError, 'argument should be a valid base64 string')
+    expect(attemptF).toThrow(TypeError, 'argument should be a valid base64 string')
   })
 })
 
 describe('function #rgbToHsl', () => {
   it('should correctly convert an RGB vector to a HSL vector', () => {
     // Arrange
-    const inputA = [0, 0, 0],     inputB = [127, 127, 127], inputC = [255, 255, 255], // white,     grey,           black,
+    const inputA = [0, 0, 0],     inputB = [127, 127, 127], inputC = [255, 255, 255], // black,     grey,           white,
           inputD = [255, 0, 0],   inputE = [0, 255, 0],     inputF = [0, 0, 255],     // red,       green,          blue,
           inputG = [178, 34, 34], inputH = [85, 107, 47],   inputI = [100, 149, 237]  // firebrick, darkolivegreen, cornflowerblue
     // Act
@@ -77,6 +89,29 @@ describe('function #rgbToHsl', () => {
     expect(resultG).toStrictEqual([0, 68, 42])
     expect(resultH).toStrictEqual([82, 39, 30])
     expect(resultI).toStrictEqual([219, 79, 66])
+  })
+
+  it('should not accept an input that is not a valid RGB vector (array of 3 integers ranging from 0 to 255)', () => {
+    // Arrange
+    const inputA = '255,255,255',     inputB = [],            inputC = [0, 0, '0'],
+          inputD = [127, 127.5, 127], inputE = [-64, 64, 64], inputF = [255, 255, 512],
+          inputG = null,              inputH = [0, 0],        inputI = [0, 0, 0, 0]
+    // Act
+    const attemptA = () => rgbToHsl(inputA), attemptB = () => rgbToHsl(inputB),
+          attemptC = () => rgbToHsl(inputC), attemptD = () => rgbToHsl(inputD),
+          attemptE = () => rgbToHsl(inputE), attemptF = () => rgbToHsl(inputF),
+          attemptG = () => rgbToHsl(inputG), attemptH = () => rgbToHsl(inputH),
+          attemptI = () => rgbToHsl(inputI)
+    // Assert
+    expect(attemptA).toThrow(TypeError,  'value should be an array')
+    expect(attemptB).toThrow(RangeError, 'value should have exactly 3 items')
+    expect(attemptC).toThrow(TypeError,  'some item in value is not an integer number')
+    expect(attemptD).toThrow(TypeError,  'some item in value is not an integer number')
+    expect(attemptE).toThrow(RangeError, 'some item in value is not a positive integer')
+    expect(attemptF).toThrow(RangeError, 'some item in value is over 255')
+    expect(attemptG).toThrow(TypeError,  'value should not be falsy/empty')
+    expect(attemptH).toThrow(RangeError, 'value should have exactly 3 items')
+    expect(attemptI).toThrow(RangeError, 'value should have exactly 3 items')
   })
 })
 
